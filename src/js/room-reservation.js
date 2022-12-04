026b196cc0027812d944c6d1acdf26ee36b20b8e
maks-vynnychenko <vinnichenkomaks@gmail.com>
2022-12-04 11:44:10 +0200

    Add js for modals

--- src/js/room-reservation.js
+++ src/js/room-reservation.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector("[room-reservation-open]"),
+    closeModalBtn: document.querySelector("[room-reservation-close]"),
+    modal: document.querySelector("[room-reservation-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
