cebb535351616ba9d49b5aff78dc4005de56e7c3
AndrewKustov1998 <49692033+AndrewKustov1998@users.noreply.github.com>
2022-12-06 11:08:13 +0000

    Deploying to gh-pages from @ AndrewKustov1998/mimino@6b0bbef9ae0ec35de2b5634bead12bd5793bbe49 🚀

---
+++ restaurant-page.503a04de.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[table-reservation-open]"),closeModalBtn:document.querySelector("[table-reservation-close]"),modal:document.querySelector("[table-reservation-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=restaurant-page.503a04de.js.map
---
+++ restaurant-page.6bf3eaed.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[room-reservation-open]"),closeModalBtn:document.querySelector("[room-reservation-close]"),modal:document.querySelector("[room-reservation-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=restaurant-page.6bf3eaed.js.map
