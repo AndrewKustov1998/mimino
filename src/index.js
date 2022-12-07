cd6b27c2bd79c4225323661ed86a81e085704734
Andrew Kustov <akustov671@gmail.com>
2022-12-07 08:34:24 +0200

    Added slider for 1 section on the home page
    It works only for desktop now

---
+++ src/index.js
@@ -0,0 +1,42 @@
+$('.one-time').slick({
+  //   dots: true,
+  prevArrow: '<button type="button" class="my-slick-prev"></button>',
+  nextArrow: '<button type="button" class="my-slick-next"></button>',
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  adaptiveHeight: true,
+});
+
+// $('.responsive').slick({
+//   dots: true,
+//   infinite: false,
+//   speed: 300,
+//   slidesToShow: 4,
+//   slidesToScroll: 4,
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 3,
+//         infinite: true,
+//         dots: true,
+//       },
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 2,
+//       },
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//       },
+//     },
+//   ],
+// });
---
+++ src/slick.js
@@ -0,0 +1,9 @@
+$('.one-time').slick({
+  dots: true,
+  prevArrow: '<button type="button" class="my-slick-prev">&xlarr;</button>',
+  nextArrow: '<button type="button" class="my-slick-next">&xrarr;</button>',
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  adaptiveHeight: true,
+});
